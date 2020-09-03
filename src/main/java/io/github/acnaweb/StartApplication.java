9a3ecf84222133b2342792bd4afa7a4a7f04c63d
AC <ac@marketmining.com.br>
2020-09-03 18:56:52 -0300

    setup Spring

---
+++ src/main/java/io/github/acnaweb/StartApplication.java
@@ -0,0 +1,11 @@
+package io.github.acnaweb;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApplication {
+
+	public static void main(String[] args) {
+
+	}
+}
