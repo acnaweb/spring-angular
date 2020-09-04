2d3b65b9452a6bba137c14056a479e8a677c7bea
AC <ac@marketmining.com.br>
2020-09-03 22:42:44 -0300

    add security

---
+++ src/main/java/io/github/acnaweb/spring_angular/spring/SecurityConfig.java
@@ -0,0 +1,9 @@
+package io.github.acnaweb.spring_angular.spring;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
---
+++ src/main/java/io/github/acnaweb/spring_angular/spring/SpringConfiguration.java
@@ -0,0 +1,8 @@
+package io.github.acnaweb.spring_angular.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfiguration {
+
+}
