c35e06c4d49dee4ffdddd64cd66a49674f759f5e
AC <ac@marketmining.com.br>
2020-09-04 01:12:44 -0300

    refactory

--- src/main/java/io/github/acnaweb/spring_angular/StartApplication.java
+++ src/main/java/io/github/acnaweb/spring_angular/StartApplication.java
@@ -1,24 +1,13 @@
 package io.github.acnaweb.spring_angular;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class StartApplication {
-
-	@Value("${application.name}")
-	private String applicationName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String helloWorld() {
-		return applicationName;
-	}
 }
